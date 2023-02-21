28be9ef4fedbd6d775458326ea874c2af45ca275
sonovrik <dmc2001@mail.ru>
2023-02-21 23:53:45 +0300

    Added basic task information for Config

--- utils/parser/ConfigParser.cpp
+++ utils/parser/ConfigParser.cpp
@@ -1,2 +1,5 @@
 #include "ConfigParser.h"
 
+ConfigParser::ConfigParser(const std::string& path_to_config) {
+	m_RootNode = YAML::Load(path_to_config);
+}
--- utils/parser/ConfigParser.h
+++ utils/parser/ConfigParser.h
@@ -2,9 +2,35 @@
 
 #include "yaml-cpp/yaml.h"
 
+#include <unordered_map>
 
 class ConfigParser
 {
+private:
+	struct TaskInfo {
+		std::string cmd;
+		int num_procs;
+		int umask;
+		std::string working_dir;
+		bool auto_start;
+		bool auto_restart;
+		std::vector<int> exit_codes;
+		std::size_t start_retries;
+		std::size_t start_time;
+		std::vector<int> stop_signals;
+		std::size_t stop_time;
 
+		std::string stdout_file;
+		std::string stderr_file;
+		std::unordered_map<std::string, std::string> env;
+	};
 
+private:
+	YAML::Node m_RootNode;
+
+public:
+	ConfigParser() = delete;
+	ConfigParser(const std::string& path_to_config);
+	~ConfigParser() = default;
 };
+
