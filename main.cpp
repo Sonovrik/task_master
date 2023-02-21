5d0f7e67dfdc86577830853097bdab15354dddd3
sonovrik <dmc2001@mail.ru>
2023-02-22 00:36:23 +0300

    Some cmake visible changes

--- main.cpp
+++ main.cpp
@@ -1,8 +1,10 @@
 #include <iostream>
 
+#include "ConfigParser.h"
 
 int main(int argc, char** argv)
 {
+	ConfigParser cf;
 	std::cout << "Hello world" << std::endl;
 
 	return 0;
--- utils/parser/ConfigParser.h
+++ utils/parser/ConfigParser.h
@@ -13,7 +13,7 @@ private:
 	TaskInfo getTask(YAML::Node& node);
 
 public:
-	ConfigParser() = delete;
+	ConfigParser() = default;
 	~ConfigParser() = default;
 
 	ConfigParser(std::string_view path_to_config);
