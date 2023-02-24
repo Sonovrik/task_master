cade4c50e61efb1b147ce881c1330753bf296d9c
sonovrik <dmc2001@mail.ru>
2023-02-24 15:27:53 +0300

    New general log methods

--- logging/Logger.h
+++ logging/Logger.h
@@ -3,10 +3,11 @@
 #include "Strategy.h"
 #include "helpers.h"
 
-
 #include <filesystem>
 #include <map>
 #include <memory>
+#include <future>
+
 namespace logging
 {
 
@@ -22,8 +23,57 @@ class Logger {
 private:
     std::unique_ptr<ILoggerStrategy> m_LogStrategy;
 
+private:
+    template<typename... TArgs>
+    void log(std::string_view level, TArgs&&... args) const
+    {
+        auto log_func = [&]()
+        {
+            std::stringstream ss;
+
+            ss << getCurrentTimeAsString() << ' ' << level << ": ";
+            ((ss << std::forward<TArgs&&>(args) << ' '), ...);
+            ss << '\n';
+
+            m_LogStrategy->write(ss);
+        };
+
+        // TODO generate only 1 thread
+        auto res = std::async(std::launch::async, log_func);
+    }
+
 public:
     Logger(LoggerType loggerType, std::string_view logs_dir, std::size_t max_log_size = GB(1));
+
+    template<typename... TArgs>
+    void info(TArgs&&... args) const
+    {
+        log("[INFO]", args...);
+    }
+
+    template<typename... TArgs>
+    void warn(TArgs&&... args) const
+    {
+        log("[WARN]", args...);
+    }
+
+    template<typename... TArgs>
+    void error(TArgs&&... args) const
+    {
+        log("[ERROR]", args...);
+    }
+
+    template<typename... TArgs>
+    void fatal(TArgs&&... args) const
+    {
+        log("[FATAL]", args...);
+    }
+
+    template<typename... TArgs>
+    void debug(TArgs&&... args) const
+    {
+        log("[DEBUG]", args...);
+    }
 
 };
 
