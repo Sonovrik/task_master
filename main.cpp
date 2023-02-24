8c69afe4870c006f7ea6435f245ac870b116cfdd
sonovrik <dmc2001@mail.ru>
2023-02-24 12:08:39 +0300

    Main parsing invoking

--- main.cpp
+++ main.cpp
@@ -4,8 +4,20 @@
 
 int main(int argc, char** argv)
 {
-	Parsing::ConfigParser cf("../config.yaml");
-	auto tmp = cf.getTasks();
+	static const std::string path_to_config = "../config";
+	std::unordered_map<std::string, utils::TaskInfo> tasks;
+	Parsing::ConfigParser cf_parser;
+	try
+	{
+		cf_parser.loadConfig(path_to_config);
+		tasks = cf_parser.getTasks();
+	}
+	catch (const std::exception& e)
+	{
+		std::cerr << "Error while parsing: " << e.what() << std::endl;
+		return -1;
+	}
+
 	std::cout << "Hello world" << std::endl;
 
 	return 0;
